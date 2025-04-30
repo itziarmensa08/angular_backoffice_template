9d29831b963c786301a12a0b63d668ff656f7471
Itziar <itziarmensaminguito@10-192-82-29client.eduroam.upc.edu>
2025-04-30 12:32:39 +0200

    list models done

--- src/app/pages/models/models.component.ts
+++ src/app/pages/models/models.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from '../../services/communication.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-models',
@@ -7,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './models.component.scss'
 })
 export class ModelsComponent {
+
+  models: string[] = [];
+
+  constructor(private comService: CommunicationService) {}
+
+  ngOnInit(): void {
+    this.getModels().subscribe(models => {
+      this.models = models;
+    });
+  }
+
+  getModels(): Observable<string[]> {
+    return this.comService.get<string[]>('/models');
+  }
 
 }
